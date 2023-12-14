b2c717a54adddc3a7e7dfae332fc372ad7eaf8cc
Andy <ndmaque@gmail.com>
2023-12-14 13:53:12 +0000

    added unit tests

---
+++ src/httpServices.test.js
@@ -0,0 +1,20 @@
+import fetchData from './httpServices';
+
+test('fetchData function should fetch data correctly', async () => {
+  // Assuming you have a mock server using something like `msw` or other mocking libraries
+  // Here, you might want to mock the fetch function to simulate a response
+  const mockResponse = { id: 'ABC', first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone: '123456789' };
+  
+  global.fetch = jest.fn().mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockResponse),
+    ok: true,
+  });
+
+  const result = await fetchData('A', 'B');
+
+  // Assertions
+  expect(result).toEqual(mockResponse);
+
+  // Make sure fetch was called with the correct URL
+  expect(global.fetch).toHaveBeenCalledWith('http://localhost:3600/api/users/A/B');
+});
